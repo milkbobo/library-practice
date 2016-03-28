226774ae0948927a013b9369374d227c14fcf7ab
“Edward” <250740918@qq.com>
2016-03-28 17:08:55 +0800

    ok

---
+++ mysql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `bname` (
+    `uid` INT(10) NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(64) NULL DEFAULT NULL,
+    `uname` VARCHAR(64) NULL DEFAULT NULL,
+    PRIMARY KEY (`uid`)
+);
